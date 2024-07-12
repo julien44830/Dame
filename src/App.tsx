99785d6652f2b70d55fbbd8702e0ff7f962fdad8
julien44830 <deniaudjulien0@gmail.com>
2024-07-12 23:58:26 +0200

    Creation d un exemple de damier et d un exemple de pions pour mettere en place la fonction drag and drop

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Damier from "./assets/components/Damier"
+import Pionts from "./assets/components/Pionts"
+import "./assets/styles/Damier.css"
+
+
+
+function App() {
+    return (
+        <main>
+            <h1>Jeu de Dame</h1>
+            <p>mon 1e jeu</p>
+            <Damier/>
+            <Pionts />
+        </main>
+    );
+}
+
+export default App;
---
+++ src/assets/components/Damier1.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Damier: React.FC = () => {
+  return (
+    <div id="damier" className="container_damier">
+    <div id="" className="1"></div>
+    <div id="" className="2"></div>
+    <div id="" className="3"></div>
+    <div id="" className="4"></div>
+  </div>
+  );
+};
+
+export default Damier;
